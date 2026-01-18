c6a0a593dc9047f20177b4f7b21cca173287fc7e
Abedalla Mohamed <abedalla.mohamed01@gmail.com>
2026-01-18 22:14:00 +0100

    API_BASE_URL fixed

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'http://localhost:8080';
 
 interface RequestOptions extends RequestInit {
   customHeaders?: Record<string, string>;
